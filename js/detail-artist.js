ff323b281f373e23792fd3e9a686d40f8b099c85
smirandaudesa <smiranda@udesa.edu.ar>
2022-06-18 14:55:53 -0300

    detail album terminado

--- js/detail-artist.js
+++
@@ -1,8 +0,0 @@
-// usar endpoinmt correcto
-// https://api.deezer.com/artist/${id} esto para nombre y foto
-// https://api.deezer.com/artist/${id}/albums esto para albums
-// un fecth adentr del otro solo queda  un catch(ultimo)
-fetch
-then
-then(fetchthenthen)
-catch
